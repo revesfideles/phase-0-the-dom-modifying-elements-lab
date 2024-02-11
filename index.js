e5e56793b26a2e1d03c5fec6a58d3e679b8d0890
ashley <revesfideles@gmail.com>
2024-02-11 11:34:13 -0500

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main").remove();
+document.createElement("h1");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "ASHLEY-KLOSKY is the champion";
